ff3e1f253da428169263aabd6a3713ccb0f57d75
Ronda <johnronda0527@gmail.com>
2025-06-18 21:07:37 +0800

    added templates

--- firstapp/app.py
+++ firstapp/app.py
@@ -1,31 +1,36 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-        return "<h1>This is the home page</h1>"
+        return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-        return "Hello World"
+# @app.route('/hello', methods=['GET', 'POST'])
+# def hello():
+#         if request.method == 'GET':
+#                 return 'You made a GET request'
+#         elif request.method == 'POST':
+#                 return 'You made a POST request'
+#         else:
+#                 return "You will not see this message."
 
-@app.route('/greet/<name>')
-def greet(name):
-        return f"Hello {name}"
+# @app.route('/greet/<name>')
+# def greet(name):
+#         return f"Hello {name}"
 
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-        return f'{number1} + {number2} = {number1 + number2}'
+# @app.route('/add/<int:number1>/<int:number2>')
+# def add(number1, number2):
+#         return f'{number1} + {number2} = {number1 + number2}'
 
-@app.route('/handle_url_params')
-def handle_params():
-        if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-                greeting = request.args['greeting']
-                name = request.args.get('name')
-                return f'{greeting}, {name}'
-        else:
-                return 'Some parameters are missing.'
+# @app.route('/handle_url_params')
+# def handle_params():
+#         if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+#                 greeting = request.args['greeting']
+#                 name = request.args.get('name')
+#                 return f'{greeting}, {name}'
+#         else:
+#                 return 'Some parameters are missing.'
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5555, debug=True)
