5562520a961e760106dcb1adf9a383efe2748dcd
Ronda <johnronda0527@gmail.com>
2025-06-25 20:57:01 +0800

    added files upload

--- firstapp/app.py
+++ firstapp/app.py
@@ -1,5 +1,7 @@
+import os
+import uuid
 import pandas as pd
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, send_from_directory
 
 app = Flask(__name__, template_folder='templates')
 
@@ -26,19 +28,76 @@ def file_upload():
                 df = pd.read_excel(file)
                 return df.to_html()
 
+@app.route('/convert_csv', methods=['POST'])
+def convert_csv():
+        file = request.files['file']
+
+        df = pd.read_excel(file)
+
+        response = Response(
+                df.to_csv(),
+                mimetype='text/csv',
+                headers={
+                        'Content-Disposition': 'attachment; filename=result.csv'
+                }
+        )
+
+        return response
+
+@app.route('/convert_csv_two', methods=['POST'])
+def convert_csv_two():
+        file = request.files['file']
+
+        df = pd.read_excel(file)
+
+        if not os.path.exists('downloads'):
+                os.makedirs('downloads')
+        
+        filename = f'{uuid.uuid4()}.csv'
+        df.to_csv(os.path.join('downloads', filename))
+
+        return render_template('download.html', filename=filename)
+
+@app.route('/download/<filename>')
+def download(filename):
+        return send_from_directory('downloads', filename, download_name='result.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/other')
 def other():
         some_text = 'Hello Worldfowinewofwe'
         return render_template('other.html', some_text=some_text)
 
-@app.template_filter('reverse_string')
-def reverse_string(s):
-        return s[::-1]
 
-@app.template_filter('repeat')
-def repeat(s, times=2):
-        return s * times
+
+
+
+
+
+
+
+
+
+# @app.template_filter('reverse_string')
+# def reverse_string(s):
+#         return s[::-1]
+
+# @app.template_filter('repeat')
+# def repeat(s, times=2):
+#         return s * times
 
 # @app.route('/hello', methods=['GET', 'POST'])
 # def hello():
