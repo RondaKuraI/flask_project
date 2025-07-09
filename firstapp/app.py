8facfb4e2d664f88a2f9f240af7d12c1c33b32e2
Ronda <johnronda0527@gmail.com>
2025-07-10 00:03:26 +0800

    added session cookies

--- firstapp/app.py
+++ firstapp/app.py
@@ -1,104 +1,87 @@
 import os
 import uuid
 import pandas as pd
-from flask import Flask, render_template, request, Response, send_from_directory, jsonify
+from flask import Flask, render_template, request, Response, send_from_directory, jsonify, session
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+app.secret_key = 'SOME KEY'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-        return render_template('index.html')
-        # if request.method == 'GET':
-        #         return render_template('index.html')
-        # elif request.method == 'POST':
-        #         username = request.form.get('username')
-        #         password = request.form.get('password')
+        return render_template('index.html', message='Index')
 
-        #         if username == 'neuralnine' and password == 'password':
-        #                 return 'Success'
-        #         else:
-        #                 return 'Failed'
-                
-@app.route('/file_upload', methods=['POST'])
-def file_upload():
-        file = request.files['file']
+@app.route('/set_data')
+def set_data():
+        session['name'] = 'Mike'
+        session['other'] = 'Hello World!'
+        return render_template('index.html', message='Session data set.')
 
-        if file.content_type == 'text/plain':
-                return file.read().decode()
-        elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
-                df = pd.read_excel(file)
-                return df.to_html()
-
-@app.route('/convert_csv', methods=['POST'])
-def convert_csv():
-        file = request.files['file']
-
-        df = pd.read_excel(file)
-
-        response = Response(
-                df.to_csv(),
-                mimetype='text/csv',
-                headers={
-                        'Content-Disposition': 'attachment; filename=result.csv'
-                }
-        )
-
-        return response
-
-@app.route('/convert_csv_two', methods=['POST'])
-def convert_csv_two():
-        file = request.files['file']
-
-        df = pd.read_excel(file)
-
-        if not os.path.exists('downloads'):
-                os.makedirs('downloads')
+@app.route('/get_data')
+def get_data():
         
-        filename = f'{uuid.uuid4()}.csv'
-        df.to_csv(os.path.join('downloads', filename))
-
-        return render_template('download.html', filename=filename)
-
-@app.route('/download/<filename>')
-def download(filename):
-        return send_from_directory('downloads', filename, download_name='result.csv')
-
-@app.route('/handle_post', methods=['POST'])
-def handle_post():
-        greeting = request.json['greeting']
-        name = request.json['name']
-
-        with open('file.txt', 'w') as f:
-                f.write(f'{greeting}, {name}')
-
-        return jsonify({'message': 'Successfully written!'})
 
 
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000, debug=True)
+                
+# @app.route('/file_upload', methods=['POST'])
+# def file_upload():
+#         file = request.files['file']
 
+#         if file.content_type == 'text/plain':
+#                 return file.read().decode()
+#         elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
+#                 df = pd.read_excel(file)
+#                 return df.to_html()
 
+# @app.route('/convert_csv', methods=['POST'])
+# def convert_csv():
+#         file = request.files['file']
 
+#         df = pd.read_excel(file)
 
+#         response = Response(
+#                 df.to_csv(),
+#                 mimetype='text/csv',
+#                 headers={
+#                         'Content-Disposition': 'attachment; filename=result.csv'
+#                 }
+#         )
 
+#         return response
 
+# @app.route('/convert_csv_two', methods=['POST'])
+# def convert_csv_two():
+#         file = request.files['file']
 
+#         df = pd.read_excel(file)
 
+#         if not os.path.exists('downloads'):
+#                 os.makedirs('downloads')
+        
+#         filename = f'{uuid.uuid4()}.csv'
+#         df.to_csv(os.path.join('downloads', filename))
 
+#         return render_template('download.html', filename=filename)
 
+# @app.route('/download/<filename>')
+# def download(filename):
+#         return send_from_directory('downloads', filename, download_name='result.csv')
 
+# @app.route('/handle_post', methods=['POST'])
+# def handle_post():
+#         greeting = request.json['greeting']
+#         name = request.json['name']
 
+#         with open('file.txt', 'w') as f:
+#                 f.write(f'{greeting}, {name}')
 
-@app.route('/other')
-def other():
-        some_text = 'Hello Worldfowinewofwe'
-        return render_template('other.html', some_text=some_text)
+#         return jsonify({'message': 'Successfully written!'})
 
-
-
-
-
-
-
-
+# @app.route('/other')
+# def other():
+#         some_text = 'Hello Worldfowinewofwe'
+#         return render_template('other.html', some_text=some_text)
 
 
 
@@ -136,5 +119,3 @@ def other():
 #         else:
 #                 return 'Some parameters are missing.'
 
-if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000, debug=True)
